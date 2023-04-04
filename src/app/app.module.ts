2f3418a73663681c7b081620993c0ca201bc5a04
zaryab444 <zaryabsohail444@gmail.com>
2023-04-04 12:59:13 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CardModule } from 'primeng/card';
+import { SalesComponent } from './sales/sales.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavigationComponent,
+    HomeComponent,
+    HeaderComponent,
+    SalesComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
     AppRoutingModule
   ],
   providers: [],
